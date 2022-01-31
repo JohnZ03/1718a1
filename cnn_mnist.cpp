14af95722abc9162e2ad65b354a05574a6ac703a
JohnZ03 <47164455+JohnZ03@users.noreply.github.com>
2022-01-31 13:31:34 -0500

    SIMD to Dense Layer 2

--- cnn_mnist.cpp
+++ cnn_mnist.cpp
@@ -171,8 +171,7 @@ void forward_pass(unsigned char img[][32]) {
         //         dense_sigmoid[i] = sigmoid(dense_sum[i]);
         // }
         for (int i=0; i<120; i+=4) {
-                __m256d v_dense_sum = _mm256_setzero_pd ();
-                __m256d v_dense_sigmoid = _mm256_setzero_pd ();
+                __m256d v_dense_sum = _mm256_setzero_pd();
                 for (int j=0; j<980; j++) {
                         __m256d v_dense_w = _mm256_load_pd(&dense_w[j][i]);
                         __m256d v_dense_input = _mm256_load_pd(&dense_input[j]);
@@ -188,12 +187,23 @@ void forward_pass(unsigned char img[][32]) {
 
         // Dense Layer 2
         // TODO: 2 Haotian
-        for (int i=0; i<10; i++) {
-                dense_sum2[i]=0;
+        // for (int i=0; i<10; i++) {
+        //         dense_sum2[i]=0;
+        //         for (int j=0; j<120; j++) {
+        //                 dense_sum2[i] += dense_w2[j][i] * dense_sigmoid[j];
+        //         }
+        //         dense_sum2[i] += dense_b2[i];
+        // }
+        for (int i=0; i<8; i+=4) {
+                __m256d v_dense_sum2 = _mm256_setzero_pd();
                 for (int j=0; j<120; j++) {
-                        dense_sum2[i] += dense_w2[j][i] * dense_sigmoid[j];
+                        __m256d v_dense_w2 = _mm256_load_pd(&dense_w2[j][i]);
+                        __m256d v_dense_sigmoid = _mm256_load_pd(&dense_sigmoid[j]);
+                        v_dense_sum2 =  _mm256_fmadd_pd(v_dense_w2, v_dense_sigmoid, v_dense_sum2);
                 }
-                dense_sum2[i] += dense_b2[i];
+                __m256d v_dense_b2 = _mm256_load_pd(&dense_b2[i]);
+                v_dense_sum2 = _mm256_add_pd(v_dense_sum2, v_dense_b2);
+                _mm256_store_pd(&dense_sum2[i], v_dense_sum2);
         }
 
         // Softmax Output
