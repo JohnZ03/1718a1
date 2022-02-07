18d036d23787f38a7d3c624a67d98525ec6e0487
Wu  Guoxian <wuguoxia@ug70.eecg.toronto.edu>
2022-02-07 01:00:01 -0500

    openclwgx

--- cnn_mnist.cpp
+++ cnn_mnist.cpp
@@ -3,7 +3,13 @@
 #include <cstdlib>
 #include <sstream>
 #include <fstream>
+#ifdef __APPLE__
+#include <OpenCL/opencl.h>
+#else
+#include <CL/cl.h>
+#endif
 
+#define MAX_SOURCE_SIZE (0x100000) 
 using namespace std;
 const int filter_size=7;
 const float eta=0.01;
@@ -177,7 +183,7 @@ void forward_pass(unsigned char img[][32]) {
         }
 }
 
-void update_weights() {
+    void update_weights() {
         // TODO: attempt on OPENCL      Lingfeng
         for (int i=0; i<120; i++) {
                 dense_b[i] -= eta*db1[i];
@@ -230,8 +236,9 @@ void backward_pass(float *y_hat, int *y, unsigned char img[][32]) {
                 }
                 delta3[i] *= d_sigmoid(dense_sum[i]);
         }
-        for (int i=0; i<120; i++) db1[i] = delta3[i]; // Bias Weight change
+        for (int i=0; i<120; i++) db1[i] = delta3[i]; // Bias Weighto change
 
+/*
         // Calculate Weight Changes for Dense Layer 1
         // TODO: attempt on OPENCL (top prio)
         for (int i=0; i<980; i++) {
@@ -239,7 +246,87 @@ void backward_pass(float *y_hat, int *y, unsigned char img[][32]) {
                         dw1[i][j] = dense_input[i]*delta3[j];
                 }
         }
+*/
 
+
+// OPENCL
+	// dense_input
+	//float *A = (float*) malloc(sizeof(float)*960);
+	// delta3
+	//float *B = (float*) malloc(sizeof(float)*120);
+	float *C = (float*) malloc(sizeof(float)*120*960);
+	int i;
+	//load the kernel source code into the array source_str
+	FILE *fp;
+	char *source_str;
+	size_t source_size;
+
+	fp = fopen("mul_kernel.cl","r");
+	if(!fp){
+	  fprintf(stderr,"Failed to load kernel.\n");
+	  exit(1);
+	}
+	source_str = (char*)malloc(MAX_SOURCE_SIZE);
+	source_size = fread(source_str,1,MAX_SOURCE_SIZE,fp);
+	fclose(fp);
+
+	// Get platform and device information
+	cl_platform_id platform_id = NULL;
+	cl_device_id device_id = NULL;
+	cl_uint ret_num_devices;
+	cl_uint ret_num_platforms;
+	cl_int ret = clGetPlatformIDs(1, &platform_id, &ret_num_platforms);
+	ret = clGetDeviceIDs( platform_id, CL_DEVICE_TYPE_ALL, 1,
+			      &device_id, &ret_num_devices);
+	cl_context context = clCreateContext(NULL, 1, &device_id, NULL, NULL, &ret);
+	cl_command_queue command_queue = clCreateCommandQueue(context,device_id,0,&ret);
+
+	// Create memory buffers on the device for each vector
+	cl_mem a_mem_obj = clCreateBuffer(context, CL_MEM_READ_ONLY,
+					  960 * sizeof(float), NULL, &ret);
+	cl_mem b_mem_obj = clCreateBuffer(context, CL_MEM_READ_ONLY,
+					  120 * sizeof(float), NULL, &ret);
+	cl_mem c_mem_obj = clCreateBuffer(context, CL_MEM_WRITE_ONLY,
+					  960 * 120 * sizeof(float), NULL, &ret);
+
+	// Create a program from the kernel source
+	cl_program program = clCreateProgramWithSource(context, 1, (const char **)&source_str, (const size_t *)&source_size, &ret);
+
+	// Build the program
+	ret = clBuildProgram(program, 1, &device_id, NULL, NULL, NULL);
+	cl_kernel kernel = clCreateKernel(program, "mul_kernel", &ret);
+	// Set the arguments of the kernel
+	ret = clSetKernelArg(kernel, 0, sizeof(cl_mem), (void *)&a_mem_obj);
+	ret = clSetKernelArg(kernel, 1, sizeof(cl_mem), (void *)&b_mem_obj);
+	ret = clSetKernelArg(kernel, 2, sizeof(cl_mem), (void *)&c_mem_obj);
+
+	// Copy the lists A and B to their respective memory buffers
+	ret = clEnqueueWriteBuffer(command_queue, a_mem_obj, CL_TRUE, 0, 960 * sizeof(float), dense_input, 0, NULL, NULL);
+	ret = clEnqueueWriteBuffer(command_queue, b_mem_obj, CL_TRUE, 0, 120 * sizeof(float), delta3, 0, NULL, NULL);
+
+	// Execute the OpenCL kernel on the list
+	size_t global_item_size = 120*960; // Process the entire list
+	size_t local_item_size = 120; // Process in groups of 120
+	ret = clEnqueueNDRangeKernel(command_queue, kernel, 1, NULL, &global_item_size, &local_item_size, 0, NULL, NULL);
+
+	ret = clEnqueueReadBuffer(command_queue, c_mem_obj, CL_TRUE, 0, 960 * 120 * sizeof(float), C, 0, NULL, NULL);
+	// Display the result to the screen
+	ret = clFlush(command_queue);
+	ret = clFinish(command_queue);
+	ret = clReleaseKernel(kernel);
+	ret = clReleaseProgram(program);
+	ret = clReleaseMemObject(a_mem_obj);
+	ret = clReleaseMemObject(b_mem_obj);
+	ret = clReleaseMemObject(c_mem_obj);
+	ret = clReleaseCommandQueue(command_queue);
+	ret = clReleaseContext(context);
+
+
+	for (int i=0; i<980; i++) {
+		for( int j=0; j <120; j++){
+			dw1[i][j] = C[i*120+j];
+		}
+	}
         // Delta2
         // TODO: attempt on OPENCL      Guoxian
         float delta2[980];
