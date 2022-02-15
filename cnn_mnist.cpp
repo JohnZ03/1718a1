ce93e8cf369c6d7dfaeb0c0b73a50e04c8050d29
JohnZ03 <47164455+JohnZ03@users.noreply.github.com>
2022-02-14 23:09:15 -0500

    Max pooling accuracy fixed

--- cnn_mnist.cpp
+++ cnn_mnist.cpp
@@ -209,37 +209,6 @@ void forward_pass(unsigned char img[][32])
 	ret = clEnqueueReadBuffer(command_queue, max_layer_mem_obj, CL_TRUE, 0,
 								sizeof(max_layer), max_layer, 0, NULL, NULL);
 
-
-	// MAX Pooling (max_pooling, max_layer)
-	// float cur_max = 0;
-	// int max_i = 0, max_j = 0;
-	// for (int filter_dim = 0; filter_dim < 5; filter_dim++)
-	// {
-	// 	for (int i = 0; i < 28; i += 2)
-	// 	{
-	// 		for (int j = 0; j < 28; j += 2)
-	// 		{
-	// 			max_i = i;
-	// 			max_j = j;
-	// 			cur_max = sig_layer[filter_dim][i][j];
-	// 			for (int k = 0; k < 2; k++)
-	// 			{
-	// 				for (int l = 0; l < 2; l++)
-	// 				{
-	// 					if (sig_layer[filter_dim][i + k][j + l] > cur_max)
-	// 					{
-	// 						max_i = i + k;
-	// 						max_j = j + l;
-	// 						cur_max = sig_layer[filter_dim][max_i][max_j];
-	// 					}
-	// 				}
-	// 			}
-	// 			max_pooling[filter_dim][max_i][max_j] = 1;
-	// 			max_layer[filter_dim][i / 2][j / 2] = cur_max;
-	// 		}
-	// 	}
-	// }
-
 	int k = 0;
 	for (int filter_dim = 0; filter_dim < 5; filter_dim++)
 	{
