e76f3368524839d83d5d060a940d393e988ff1ad
JohnZ03 <47164455+JohnZ03@users.noreply.github.com>
2022-03-01 21:57:27 -0500

    create hwacc

---
+++ hwacc.h
@@ -0,0 +1,11 @@
+#ifndef HWACC_DOT_H
+#define HWACC_DOT_H
+
+/**
+ * This is a function declaration.
+ * It tells the compiler that the function exists somewhere.
+ */
+void foo(int id, char *name);
+
+
+#endif
