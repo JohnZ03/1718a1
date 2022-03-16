05279602372b9ccabe988a18744a0e180f0fec83
wugxfly <735374543@qq.com>
2022-03-16 13:26:31 -0700

    dense2_top modified

--- dense/dense2/dut/dense2_top.v
+++ dense/dense2/dut/dense2_top.v
@@ -11,7 +11,7 @@ module dense2_top(
 
 wire [9:0] valid_10;
 
-wire [9:0]    rom0_addr;
+wire [9:0]    rom0_addr[9:0];
 wire [159:0]  rom0_data;
 
 reg  [15:0]   dense_b2[9:0];
@@ -30,7 +30,7 @@ generate
             .dense_input      (dense_sigmoid ),   
             .valid            (valid_10[i]),
             .dense_sum_out    (dense_sum2_mid[i*16+15:i*16]),
-            .rom0_addr        (rom0_addr),
+            .rom0_addr        (rom0_addr[i]),
             .rom0_data        (rom0_data[i*16+15:i*16])
         );
         always @ (posedge clk or negedge rst_n)
@@ -52,7 +52,7 @@ dense_w2_rom_inst
 (
     .clk  (clk),
     .ena  (ena),
-    .addr (rom0_addr[7:0]),
+    .addr (rom0_addr[0][7:0]),
     .q    (rom0_data)
     );
 
